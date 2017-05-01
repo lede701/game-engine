b1a002a826c5e9c21a4ee7843f2e680fae306eec
Leland Ede <lede@zaxis-studios.com>
2017-04-30 21:52:07 -0700

    Made changes to the core engine class and started working on the entity class.

---
+++ src/js/core/entity.js
@@ -0,0 +1,27 @@
+﻿function entity(cfg) {
+	var me = this;
+
+	if (cfg !== undefined) {
+		me.init(cfg);
+	}
+
+	me.init = function(cfg){
+		Object.assign(cfg);
+	};
+
+	me.update = function (deltaTime) {
+
+	};
+
+	me.draw = function (ctx) {
+		// Default entity has nothing to draw
+	};
+
+	me.engineDraw = function (ctx) {
+		// Need to perform all translation and rotation to entity
+
+		// Call the entity draw method
+
+		// Restore context
+	};
+}
